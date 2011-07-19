5879e305c2f733564eebd9bec2547e8399222c8a
Tejaswi <rao.thejaswi@gmail.com>
2011-07-19 07:36:39 +0530

    First commit

---
+++ README.txt
@@ -0,0 +1,26 @@
+
+              *** SD card speed test **
+
+
+0. Introduction:
+ This is a very handy script to measure the bandwidth of the sd-card on your
+android phones. You just need to plug your phone with USB cable to a PC and
+run the 'appropriate' script.
+
+
+1. How to run?
+ Decide what script you will need to run using the table below:
+  a. On Linux:   use the script 'sdCardSpeedTest.sh'.
+  b. On Windows: use the script 'sdCardSpeedTest.bat'.
+  c. On Mac:     TODO
+  d. Else:       TODO!
+Then, run that script with the following syntax:
+  <script> <sdcardlocation>
+Where:
+ <sdcardlocation> is the path to the sdcard on whom you want to measure the
+write/read BW. For eg: /mnt/sdcard.
+
+
+2. License:
+ This is released with WTFPL (do What The Fuck you want to Public License).
+For details on this license refer to this link: http://sam.zoy.org/wtfpl/
---
+++ sdCardSpeedTest.sh
@@ -0,0 +1,112 @@
+#!/bin/bash
+#
+# COMMENTS:
+#   A very handy script to measure the read/write BW of the sd-card
+# (internal/external) of your android phone.
+#
+# USAGE:
+#  sdCardSpeedTest.sh <sdCardLocation>
+#   <sdCardLocation>    The location where your sdcard can be found
+#                       in your phone. Eg: /mnt/sdcard
+#
+
+
+
+function fatal() {
+    local msg=$1
+    echo "**FATAL** $msg"
+    exit 1
+}
+
+
+function bwTest() {
+    local cacheFile=$1
+    local numTests=$2
+    local blkSize=$3
+    local blkCnt=$4
+    local IF=$5
+    local OF=$6
+    # disable caching
+    echo "echo 1 > $cacheFile"
+    # actual test
+    for i in `seq 1 $numTests`; do
+        echo "dd if=$IF of=$OF bs=$blkSize count=$blkCnt 2>&1 | grep bytes"
+    done
+    # restore original cache value
+    echo "echo 0 > $cacheFile"
+}
+
+function runBwTest() {
+    local pcScript=$1
+    local adbScript=$2
+    local log=$3
+    $ADB push $pcScript $adbScript &>/dev/null
+    $ADB shell chmod +x $adbScript
+    $ADB shell $adbScript | tee $log
+}
+
+function evalBW() {
+    local log=$1
+    sed -e 's/(//' $log | awk 'BEGIN{bw=0;num=0} {bw+=$7;num++} END{print bw/1048576/num " MBps"}'
+}
+
+function clean() {
+    local adbFiles=$1
+    local pcFiles=$2
+    $ADB shell rm $adbFiles
+    rm $pcFiles
+}
+
+
+
+
+### MAIN SCRIPT ###
+export PATH=.:$PATH
+# constants
+OS_TYPE=`uname -o`
+if [ "$OS_TYPE" = "GNU/Linux" ]; then
+    ADB=./adb-linux
+elif [ "$OS_TYPE" = "Cygwin" ]; then
+    ADB=./adb-windows.exe
+else
+    fatal "Currently, this script is supported only for 'Linux' and 'Cygwin'!"
+fi
+CACHE=/proc/sys/vm/drop_caches
+BLK_SIZ=100000
+BLK_CNT=1000
+NUM_RUNS=6
+WR_SCRIPT=sd-wr-speed.sh
+WR_TST_SH=/data/local/sd-wr-speed.sh
+WR_LOG=sd-wr-speed.log
+RD_SCRIPT=sd-rd-speed.sh
+RD_TST_SH=/data/local/sd-rd-speed.sh
+RD_LOG=sd-rd-speed.log
+
+# user inputs and derivatives
+sdcard=$1
+if [ "$sdcard" = "" ]; then
+    fatal "USAGE: ./sdCardSpeedTest.sh <sdcardLocation>"
+fi
+TST_FILE=$sdcard/empty.file
+
+
+
+# write test
+echo "Evaluating write-BW..."
+bwTest $CACHE $NUM_RUNS $BLK_SIZ $BLK_CNT /dev/zero $TST_FILE > $WR_SCRIPT
+runBwTest $WR_SCRIPT $WR_TST_SH $WR_LOG
+echo -n "Write BandWidth: "
+evalBW $WR_LOG
+echo
+
+# read test
+echo "Evaluating read-BW..."
+bwTest $CACHE $NUM_RUNS $BLK_SIZ $BLK_CNT $TST_FILE /dev/null > $RD_SCRIPT
+runBwTest $RD_SCRIPT $RD_TST_SH $RD_LOG
+echo -n "Read BandWidth: "
+evalBW $RD_LOG
+echo
+
+# clean-up
+echo "Cleaning up all the temporary files..."
+clean "$WR_TST_SH $RD_TST_SH $TST_FILE" "$WR_SCRIPT $RD_SCRIPT $WR_LOG $RD_LOG"
